938b3c10dcbb17d5bb28fd0ec36c6a76598f874e
Vicente Mundim <vicente.mundim@gmail.com>
2013-04-07 17:43:58 -0300

    Adding g-zip support for assets

--- config/application.rb
+++ config/application.rb
@@ -70,5 +70,7 @@ module LiveEstimationPoker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
   end
 end
