12ca1716a9170f16c93dd7dcbb932ccd2f7679be
Vicente Mundim <vicente.mundim@gmail.com>
2014-05-28 12:06:31 -0300

    Allowing name parameter on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
 
   protect_from_forgery
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 end
